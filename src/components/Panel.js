5e48e4440552265c4ba4e1b959c3bc62a7140600
alperkirkus <alperkirkus@gmail.com>
2021-06-08 04:33:57 +0300

    home page writing changed

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,37 +1,24 @@
-import React ,{useState}from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
 
-import Navs from '../components/Navs'
-import CodeGen from './CodeGen'
-function Panel({setUser}) {
+import Navs from "../components/Navs";
+import CodeGen from "./CodeGen";
+function Panel({ setUser }) {
+  const [route, setRoute] = useState("home");
 
-    const [route,setRoute] = useState("home")
-
-    const renderMenu = ()=>{
-
-        if(route === "home")
-        {
-            return(
-                <div>home her....</div>
-            )
-        }
-        else if(route === "code-gen")
-        {
-            return(
-                <CodeGen/>
-            )
-        }
+  const renderMenu = () => {
+    if (route === "home") {
+      return <div>This is the homepage for admin</div>;
+    } else if (route === "code-gen") {
+      return <CodeGen />;
     }
-    return (
-        <>
-        <Navs setRoute={setRoute} setUser ={setUser}/>
-        <Container>
-         
-        {renderMenu()}
-        </Container>
-        </>
-        
-    )
+  };
+  return (
+    <>
+      <Navs setRoute={setRoute} setUser={setUser} />
+      <Container>{renderMenu()}</Container>
+    </>
+  );
 }
 
-export default Panel
+export default Panel;
