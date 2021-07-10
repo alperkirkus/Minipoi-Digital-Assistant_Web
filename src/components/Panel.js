a4094429815fbb000f3678b7f897d3c9c8278604
alperkirkus <alperkirkus@gmail.com>
2021-07-10 22:50:18 +0300

    last changes

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,51 +1,30 @@
-import React ,{useState}from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
 
-import Navs from '../components/Navs'
-import AddBook from './AddBook'
-import AddExercises from './AddExercises'
-import CodeGen from './CodeGen'
-function Panel({setUser}) {
+import Navs from "../components/Navs";
+import AddBook from "./AddBook";
+import AddExercises from "./AddExercises";
+import CodeGen from "./CodeGen";
+function Panel({ setUser }) {
+  const [route, setRoute] = useState("home");
 
-    const [route,setRoute] = useState("home")
-
-    const renderMenu = ()=>{
-
-        if(route === "home")
-        {
-            return(
-                <div>home her....</div>
-            )
-        }
-        else if(route === "code-gen")
-        {
-            return(
-                <CodeGen/>
-            )
-        }
-        else if(route === 'add-book')
-        {
-            return(
-                <AddBook/>
-            )
-        }
-        else if(route === 'add-ex')
-        {
-            return(
-                <AddExercises/>
-            )
-        }
+  const renderMenu = () => {
+    if (route === "home") {
+      return <div>This is the home page</div>;
+    } else if (route === "code-gen") {
+      return <CodeGen />;
+    } else if (route === "add-book") {
+      return <AddBook />;
+    } else if (route === "add-ex") {
+      return <AddExercises />;
     }
-    return (
-        <>
-        <Navs setRoute={setRoute} setUser ={setUser}/>
-        <Container>
-         
-        {renderMenu()}
-        </Container>
-        </>
-        
-    )
+  };
+  return (
+    <>
+      <Navs setRoute={setRoute} setUser={setUser} />
+      <Container>{renderMenu()}</Container>
+    </>
+  );
 }
 
-export default Panel
+export default Panel;
