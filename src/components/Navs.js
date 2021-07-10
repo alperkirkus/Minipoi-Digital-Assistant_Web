3492e9171bb600489b8a68a3fb80b41085174a70
alperkirkus <alperkirkus@gmail.com>
2021-07-10 23:21:07 +0300

    last changes

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
-function Navs({setRoute,setUser}) {
-
-  const logOut = ()=>{
-    setUser(null)
-    localStorage.removeItem("user")
-  }
-    return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-       
-          <Navbar.Brand><img alt="logo" className="nav-logo" src={logo} /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link onClick ={()=>setRoute("home")}>Home</Nav.Link>
-              <Nav.Link onClick ={()=>setRoute("add-book")}>Add Book</Nav.Link>
-              <Nav.Link onClick ={()=>setRoute("add-ex")}>Add Exercise</Nav.Link>
-              <Nav.Link onClick ={()=>setRoute("code-gen")}>Code Generator</Nav.Link>
-              <Nav.Link onClick ={logOut}>Çıkış</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-      
-      </Navbar>
-    )
+function Navs({ setRoute, setUser }) {
+  const logOut = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand>
+        <img alt="logo" className="nav-logo" src={logo} />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link onClick={() => setRoute("home")}>Home</Nav.Link>
+          <Nav.Link onClick={() => setRoute("add-book")}>Add Book</Nav.Link>
+          <Nav.Link onClick={() => setRoute("add-ex")}>Add Exercise</Nav.Link>
+          <Nav.Link onClick={() => setRoute("code-gen")}>
+            Code Generate
+          </Nav.Link>
+          <Nav.Link onClick={logOut}>Log out</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default Navs
+export default Navs;
